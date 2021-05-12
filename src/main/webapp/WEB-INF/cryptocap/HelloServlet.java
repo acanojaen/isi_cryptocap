5bde395aa2b84c2f8c447fe2b9a0f4efb70fb631
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-12 20:52:22 +0200

    0.0.1 relocate

---
+++ src/main/webapp/WEB-INF/cryptocap/HelloServlet.java
@@ -0,0 +1,31 @@
+package main.webapp.cryptocap;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(
+        name = "HelloServlet",
+        urlPatterns = {"/hello"}
+    )
+public class HelloServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
+    }
+}

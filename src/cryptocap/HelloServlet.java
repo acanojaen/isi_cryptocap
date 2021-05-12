c511e3f9ace52b46da1f9537f10222a5ce3bae37
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-12 19:44:38 +0200

    Fix Servlet

--- src/cryptocap/HelloServlet.java
+++ src/cryptocap/HelloServlet.java
@@ -1,16 +1,14 @@
 package cryptocap;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(
-	    name = "HelloServlet",
-	    urlPatterns = {"/hello"}
-	)
-
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet
 {
 
@@ -19,13 +17,13 @@ public class HelloServlet extends HttpServlet
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
 		// Set response content type
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		// Respuesta del servlet
-		response.getWriter().println("Hello World! Servlet working");
+		response.getWriter().println("<h2>Hello World! Servlet working</h2>");
 	}
 }
 

367f2007f3d69a935313b68309b908a63a48bad1
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-12 19:03:42 +0200

    Maven: Javax.Servlet

---
+++ src/cryptocap/HelloServlet.java
@@ -0,0 +1,19 @@
+package cryptocap;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*; 
+
+public class HelloServlet extends HttpServlet
+{
+
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// Set response content type
+		response.setContentType("text/html");
+		// Respuesta del servlet
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+}
+

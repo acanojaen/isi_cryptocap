3ed1205fc89bfcaf255a7dc34bff3b1003b34d48
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-12 20:50:51 +0200

    0.0.1

---
+++ src/main/webapp/cryptocap/criptomoneda.java
@@ -0,0 +1,96 @@
+package main.webapp.cryptocap;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class criptomoneda 
+{	
+	String nombre;
+	float volumen;
+	float capitalizacion;
+	float suministro_circulacion;
+	int rango;
+	String descripcion;
+	Date fecha_emision;
+	String web;
+	ArrayList<HistorialPrecios> historial;
+
+	public criptomoneda(String nombre, float volumen, float capitalizacion, float suministro_circulacion, int rango, String descripcion, Date fecha_emision, String web) {
+		this.historial = new ArrayList<HistorialPrecios>();
+		this.nombre = nombre;
+		this.volumen = volumen;
+		this.capitalizacion = capitalizacion;
+		this.suministro_circulacion = suministro_circulacion;
+		this.rango = rango;
+		this.descripcion = descripcion;
+		this.fecha_emision = fecha_emision;
+		this.web = web;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(float volumen) {
+		this.volumen = volumen;
+	}
+
+	public float getCapitalizacion() {
+		return capitalizacion;
+	}
+
+	public void setCapitalizacion(float capitalizacion) {
+		this.capitalizacion = capitalizacion;
+	}
+
+	public float getSuministro_circulacion() {
+		return suministro_circulacion;
+	}
+
+	public void setSuministro_circulacion(float suministro_circulacion) {
+		this.suministro_circulacion = suministro_circulacion;
+	}
+
+	public int getRango() {
+		return rango;
+	}
+
+	public void setRango(int rango) {
+		this.rango = rango;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFecha_emision() {
+		return fecha_emision;
+	}
+
+	public void setFecha_emision(Date fecha_emision) {
+		this.fecha_emision = fecha_emision;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	
+	
+
+}

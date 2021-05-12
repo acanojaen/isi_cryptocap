0005705be94ad8715ea9614566bf7a07065d9709
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-12 21:43:22 +0200

    0.0.0.1

--- src/cryptocap/HelloServlet.java
+++ src/cryptocap/HelloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 
-
+@WebServlet(name="HelloServlet", value = "/hello") 
 public class HelloServlet extends HttpServlet {
 
     /**

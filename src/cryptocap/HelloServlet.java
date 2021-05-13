c29b6dab8f21977b7b8938435e60b7538307e8ba
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-13 09:43:35 +0200

    0.0.2: Servlet

--- src/cryptocap/HelloServlet.java
+++ src/cryptocap/HelloServlet.java
@@ -3,7 +3,6 @@ package cryptocap;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,11 +17,10 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
+        response.setContentType("text/plan");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println("Hello App working!\r\n");
     }
 }

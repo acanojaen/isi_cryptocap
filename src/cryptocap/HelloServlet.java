ad35dd522e3ca54e88e55fe294abbd0feb2548cb
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-18 21:02:57 +0200

    0.0.2

--- src/cryptocap/HelloServlet.java
+++ src/cryptocap/HelloServlet.java
@@ -1,7 +1,7 @@
 package cryptocap;
 
 import java.io.IOException;
-
+import com.google.gson.Gson;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,15 +19,15 @@ public class HelloServlet extends HttpServlet {
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/plan");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().println("Hello App working!\r\n");
+        response.setContentType("application/json");
         Webscraping w;
         Criptomoneda c;
         w = new Webscraping("BTC");
         c = new Criptomoneda(w.Binance("BTC"));
+        Gson gson = new Gson();
+        String json = gson.toJson(c);
 
-        response.getWriter().println(c.getNombre());
+        response.getWriter().println(json);
     }
 
 }

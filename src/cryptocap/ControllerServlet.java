74efc2578793434045c3b0a2f58df2bb06b29a95
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-19 16:34:26 +0200

    0.0.4: Test base de datos
    jdbc añadido (probando sin fichero)

---
+++ src/cryptocap/ControllerServlet.java
@@ -0,0 +1,55 @@
+package cryptocap;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private CriptomonedaDAO c;
+
+    /**
+	 * Inicializar valores de la base de datos
+     * están definidos en web.xml
+	 */
+    public void init() {
+        String jdbcURL = "jdbc:mysql://lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/fbj6jfrdn8y5rorn?serverTimezone=" + TimeZone.getDefault().getID();
+		String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+		String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+
+        setCore(new CriptomonedaDAO(jdbcURL, jdbcUsername, jdbcPassword));
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String elegido = request.getServletPath();
+
+        try {
+            switch(elegido){
+                case "/coinmarket":
+                    getScrap(request, response, "coinmaket")
+                    break;
+            }
+
+
+        } catch ()
+    }
+
+    public void main(HttpServletRequest request, HttpServletResponse response, String site){
+        CriptomonedaDAO.test();
+    }
+
+    public void setCore(CriptomonedaDAO c){
+        this.c = c;
+    }
+}
---
+++ src/cryptocap/CriptomonedaDAO.java
@@ -0,0 +1,58 @@
+package cryptocap;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * CriptomonedaDAO.java
+ * Operaciones CRUD sobre el TDA Criptomoneda.
+ */
+
+public class CriptomonedaDAO 
+{	
+    private String jdbcURL;
+    private String jdbcUsername;
+    private String jdbcPassword;
+    private Connection jdbcConnection;
+    static ResultSet res = null;
+    
+    public CriptomonedaDAO (String jdbcURL, String jdbcUsername, String jdbcPassword){
+        this.jdbcURL = jdbcURL;
+        this.jdbcUsername = jdbcUsername;  
+        this.jdbcPassword = jdbcPassword;
+    }
+
+    protected void connect() throws SQLException {
+        if (jdbcConnection == null || jdbcConnection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver"); 
+            } catch (ClassNotFoundException e){
+                throw new SQLException(e);
+            }
+
+            jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword)
+        }
+    }
+
+    protected void disconnect() throws SQLException {
+        if (jdbcConnection != null && !jdbcConnection.isClosed()){
+            jdbcConnection.close();
+        }
+    }
+
+    public boolean test(Criptomoneda c) throws SQLException {
+        connect();
+
+        
+        disconnect();
+    }
+
+
+}

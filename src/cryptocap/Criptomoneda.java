c632cfda924d3c1386f39ef7fd0429636cf71a31
Alberto Cano Jaén <43446118+acanojaen@users.noreply.github.com>
2021-05-20 21:40:32 +0200

    Update Criptomoneda.java

--- src/cryptocap/Criptomoneda.java
+++ src/cryptocap/Criptomoneda.java
@@ -60,16 +60,7 @@ public class Criptomoneda {
 		this.ultimaActualizacion = ultAct;
 	}
 
-	public Criptomoneda(String nom, String acron, String imag, String url, String ultAct){
-		this.nombre = nom;
-		this.acronimo = acron;
-		this.imagen = imag;
-		this.urlDatos = url;
-
-		this.ultimaActualizacion = ultAct;
-	}
-
-	public Criptomoneda(String prec, String capMer, String vol24, String volTotal, String var24, String var7, String ultAct){
+	Criptomoneda(String prec, String capMer, String vol24, String volTotal, String var24, String var7, String ultAct){
 		this.precio = prec;
 		this.capMercado = capMer;
 		this.volumen24 = vol24;
@@ -79,132 +70,107 @@ public class Criptomoneda {
 
 		this.ultimaActualizacion = ultAct;
 	}
-
+	
+	public Criptomoneda(String nombre, String acronimo, String imagen, String urlDatos, String ultimaActualizacion) {
+		this.nombre = nombre;
+		this.acronimo = acronimo;
+		this.imagen = imagen;
+		this.urlDatos = urlDatos;
+		this.ultimaActualizacion = ultimaActualizacion;
+	}
+	
 	/*-----------------------------------------Modificadores-----------------------------------------*/
-
-	public void setNombre(String nom){
-		this.nombre = nom;
-	}
-
-	public void setAcronimo(String acron){
-		this.acronimo = acron;
-	}
-
-	public void setImagen(String imag){
-		this.imagen = imag;
-	}
-
-	public void setUrlDatos(String url){
-		this.urlDatos = url;
-	}
-
-	public void setPrecio(String prec){
-		this.precio = prec;
-	}
-
-	public void setCapMercado(String capMer){
-		this.capMercado = capMer;
-	}
-
-	public void setVolumen24(String vol24){
-		this.volumen24 = vol24;
-	}
-
-	public void setVolumenTotal(String volTotal){
-		this.volumenTotal = volTotal;
-	}
-
-	public void setVariacion24(String var24){
-		this.variacion24 = var24;
-	}
-
-	public void setVariacion7(String var7){
-		this.variacion7 = var7;
-	}
-
-	public void setUltimaActualizacion(String ultAct){
-		this.ultimaActualizacion = ultAct;
-	}
-
-	public void set(String nom, String acron, String imag, String url, String prec, String capMer,
-				 String vol24, String volTotal, String var24, String var7){
-		this.nombre = nom;
-		this.acronimo = acron;
-		this.imagen = imag;
-		this.urlDatos = url;
-
-		this.precio = prec;
-		this.capMercado = capMer;
-		this.volumen24 = vol24;
-		this.volumenTotal = volTotal;
-		this.variacion24 = var24;
-		this.variacion7 = var7;
-	}
-
-	public void set(String nom, String acron, String imag, String url){
-		this.nombre = nom;
-		this.acronimo = acron;
-		this.imagen = imag;
-		this.urlDatos = url;
-	}
-
-	public void set(String prec, String capMer, String vol24, String volTotal, String var24, String var7){
-		this.precio = prec;
-		this.capMercado = capMer;
-		this.volumen24 = vol24;
-		this.volumenTotal = volTotal;
-		this.variacion24 = var24;
-		this.variacion7 = var7;
-	}
-
-	/*------------------------------------------Consultores------------------------------------------*/
-
-	public String getNombre(){
+	
+	public String getNombre() {
 		return nombre;
 	}
 
-	public String getAcronimo(){
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAcronimo() {
 		return acronimo;
 	}
 
-	public String getImagen(){
+	public void setAcronimo(String acronimo) {
+		this.acronimo = acronimo;
+	}
+
+	public String getImagen() {
 		return imagen;
 	}
 
-	public String getUrlDatos(){
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getUrlDatos() {
 		return urlDatos;
 	}
 
-	public String getPrecio(){
+	public void setUrlDatos(String urlDatos) {
+		this.urlDatos = urlDatos;
+	}
+
+	public String getPrecio() {
 		return precio;
 	}
 
-	public String getCapMercado(){
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getCapMercado() {
 		return capMercado;
 	}
 
-	public String getVolumen24(){
+	public void setCapMercado(String capMercado) {
+		this.capMercado = capMercado;
+	}
+
+	public String getVolumen24() {
 		return volumen24;
 	}
 
-	public String getVolumenTotal(){
+	public void setVolumen24(String volumen24) {
+		this.volumen24 = volumen24;
+	}
+
+	public String getVolumenTotal() {
 		return volumenTotal;
 	}
 
-	public String getVariacion24(){
+	public void setVolumenTotal(String volumenTotal) {
+		this.volumenTotal = volumenTotal;
+	}
+
+	public String getVariacion24() {
 		return variacion24;
 	}
 
-	public String getVariacion7(){
+	public void setVariacion24(String variacion24) {
+		this.variacion24 = variacion24;
+	}
+
+	public String getVariacion7() {
 		return variacion7;
 	}
 
-	public String getUltimaActializacion(){
+	public void setVariacion7(String variacion7) {
+		this.variacion7 = variacion7;
+	}
+
+	public String getUltimaActualizacion() {
 		return ultimaActualizacion;
 	}
 
-	/*-------------------------------------------to_String-------------------------------------------*/
+	public void setUltimaActualizacion(String ultimaActualizacion) {
+		this.ultimaActualizacion = ultimaActualizacion;
+	}
 
+	/*-------------------------------------------to_String-------------------------------------------*/
+	
 	@Override
 	public String toString() {
 		return "{imagen" + imagen + "nombre : " + nombre + ", acronimo : " + acronimo + ", precio : " + precio + 

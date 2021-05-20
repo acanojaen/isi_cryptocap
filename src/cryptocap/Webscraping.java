2c2cd29c0ec2f2c793bc550d1d085101ec70ea8a
germanca8 <germanca8@users.noreply.github.com>
2021-05-20 20:00:35 +0200

    Web scraping con 2 fuentes de datos diferentes (Version 1)

--- src/cryptocap/Webscraping.java
+++ src/cryptocap/Webscraping.java
@@ -16,10 +16,15 @@ public class Webscraping {
 	
 	String nombre;
 	String acronimo;
+	String imagen;
+	String urlDatos;
+
 	String precio;
 	String capitalizacion;
-	String urlDatos;
-	String ultAct;
+	String vol24;
+	String volTotal;
+	String 24hchange;
+	String 7dchange;
 
 	Webscraping () {
 		this.nombre = "";
@@ -37,21 +42,19 @@ public class Webscraping {
 			// cargamos el html de la pï¿½gina
 			Document doc = html(url);
 			
-			// obtenemos la lista de las criptomonedas (1 página)
+			// obtenemos la lista de las criptomonedas (1 pï¿½gina)
 			Elements element = doc.select(":not(thead) tr.table__row.table__row--click.table__row--full-width");
 			
 			// recorremos todas las criptomonedas
 			for (Element elem : element) {
 				nombre = elem.getElementsByClass("profile__link").text();
 				acronimo = elem.getElementsByClass("profile__subtitle").text();
-				precio = elem.getElementsByClass("valuta valuta--light").text();
-				capitalizacion = elem.getElementsByClass("valuta valuta--light").text();
-				urlDatos = url + elem.getElementsByClass("profile__link").attr("href");
-				ultAct = getActualHour();
+				imagen = elem.getElementsByClass("profile__logo-background").text();
+				urlDatos = url + elem.getElementsByClass("profile__link").attr("href").text();
 
 				// buscamos la que nosotros queremos
 				if(elem.getElementsByClass("profile__subtitle").text().equals(acron)){
-					return (new Criptomoneda(nombre, acronimo, precio, capitalizacion, urlDatos, ultAct));
+					return (new Criptomoneda(nombre, acronimo, imagen, urlDatos));
 				}
 			}
 			
@@ -59,10 +62,51 @@ public class Webscraping {
 			return (new Criptomoneda("No se ha encontrado la moneda " + acron));
 			
 		} else {
-			// si el codigo no es 200 (éxito)
+			// si el codigo no es 200 (ï¿½xito)
 			return (new Criptomoneda("Codigo != 200"));
 		}
-	}		
+	}	
+	
+	// Scraping - Investing.com/es
+	public Criptomoneda Investing(String acron) throws IOException {
+		String url2 = "https://es.investing.com/crypto/currencies";
+		int code;
+		
+		code = connect(url2);
+		
+		// acceso correcto
+		if(code == 200) {
+			// cargamos el html de la pï¿½gina
+			Document doc = html(url2);
+			
+			//***********************CAMBIAR doc.select*****************
+			// obtenemos la lista de las criptomonedas (1 pï¿½gina)
+			Elements element = doc.select(":not(thead) tr.table__row.table__row--click.table__row--full-width");
+			
+			// recorremos todas las criptomonedas
+			for (Element elem : element) {
+				precio = elem.getElementsByClass("price js-currency-price").text();
+				capitalizacion = elem.getElementsByClass("js-market-cap").text();
+				vol24 = elem.getElementsByClass("js-24h-volume").text();
+				volTotal = elem.getElementsByClass("js-total-vol").text();
+				24hchange = elem.getElementsByClass("js-currency-change-24h redFont pid-1057391-pcp").text();
+				7dchange = elem.getElementsByClass("js-currency-change-7d redFont").text();
+
+				//***********************CAMBIAR new Criptomoneda*****************
+				// buscamos la que nosotros queremos  
+				if(elem.getElementsByClass("left noWrap elp symb js-currency-symbol").text().equals(acron)){
+					return (new Criptomoneda(precio, capitalizacion, vol24, volTotal, 24hchange, 7dchange));
+				}
+			}
+			
+			// si no se encuentra
+			return (new Criptomoneda("No se ha encontrado la moneda " + acron));
+			
+		} else {
+			// si el codigo no es 200 (ï¿½xito)
+			return (new Criptomoneda("Codigo != 200"));
+		}
+	}
 	
 
 	public static int connect(String url) throws IOException {
